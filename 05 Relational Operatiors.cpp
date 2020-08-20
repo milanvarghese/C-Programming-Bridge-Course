75bb9ff876c1954d5dbd018c7536adc465854543
Milan <35422302+milanvarghese@users.noreply.github.com>
2020-08-20 11:28:25 +0530

    Add files via upload

---
+++ 05 Relational Operatiors.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int i,j,k;
+	i=2;
+	j=10;
+	if(i>j)
+	printf("%d > %d\n",i,j);
+	return 0;
+}
---
+++ 06 Bitwise Operator.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int i,j,k;
+	i=15;
+	j=5;
+	k=i&j;
+	printf("The result of %d & %d = %d\n",i,j,k);
+	k=i|j;
+	printf("The result of %d | %d = %d\n",i,j,k);
+	k=i^j;
+	printf("The result of %d ^ %d = %d\n",i,j,k);
+	return 0;
+}
